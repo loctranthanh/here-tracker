4f3bb90ae1b6e7b09c8d8513410689a42df71cf5
Loc Tran Thanh <loctranthanh36@gmail.com>
2018-11-18 07:29:14 +0700

    show map, add qr source

---
+++ components/qr/component.mk
@@ -0,0 +1,4 @@
+
+COMPONENT_ADD_INCLUDEDIRS := . ./QRCode/src
+COMPONENT_SRCDIRS := QRCode/src
+
--- main/app_main.c
+++ main/app_main.c
@@ -28,12 +28,30 @@
 #include <sys/time.h>
 #include "here_tracking_oauth.h"
 #include "json_utils.h"
+#include "tftspi.h"
+#include "tft.h"
+
+#define SPI_BUS TFT_HSPI_HOST
 
 static const char *TAG = "TRACKER_HERE";
 // #define METHOD_PPP              1
 #define MAX_HTTP_RECV_BUFFER    512
+#define MAP_TEMPLATE            "https://image.maps.api.here.com/mia/1.6/tiltmap?app_id=4SOW7wtvnVNhniJUSFRr&app_code=LAQ7xdmuHxFmTlPhb_8Heg&w=320&h=240&sb=mk&z=15&t=2&u=100&c=%s,%s"
+
+#define _mutex_lock(x)      while (xSemaphoreTake(x, portMAX_DELAY) != pdPASS)
+#define _mutex_unlock(x)    xSemaphoreGive(x)
+#define _mutex_create()     xSemaphoreCreateMutex()
+#define _mutex_destroy(x)   vSemaphoreDelete(x)
 
 esp_ppp_handle_t ppp_handle = NULL;
+static int state_version = 0;
+static struct tm* tm_info;
+static char tmp_buff[64];
+static time_t time_now, time_last = 0;
+
+char *access_token;
+
+SemaphoreHandle_t lcd_lock;
 
 static void initialize_sntp(void)
 {
@@ -49,11 +67,53 @@ char auth_header_here[512];
 char http_buffer[4096];
 int http_data_len = 0;
 
+uint8_t http_map_buffer[40000];
+uint16_t http_map_data_len;
+
 esp_err_t _http_event_handler(esp_http_client_event_t *evt)
 {
     esp_http_client_handle_t client = evt->client;
-    const char *device_id = "a2abff51-5313-440d-a05f-7f7fcb20a0f9";
-    const char *device_secret = "RQJbb6Ij8CTj2pDLhA-IRKMMk16gom0JQvGoN-j2pe0";
+    const char *device_id = "567c0e98-6f60-4f24-a7dc-e5669636c649";
+    const char *device_secret = "mqqk5t7AS4-KVS5aRaPQDpHmmj8ml8b-a8oMdmFkOOw";
+    uint32_t sz = 512;
+
+    switch (evt->event_id) {
+        case HTTP_EVENT_ERROR:
+            ESP_LOGD(TAG, "HTTP_EVENT_ERROR");
+            break;
+        case HTTP_EVENT_ON_CONNECTED:
+            break;
+        case HTTP_EVENT_HEADER_SENT:
+            ESP_LOGD(TAG, "HTTP_EVENT_HEADER_SENT");
+            break;
+        case HTTP_EVENT_ON_HEADER:
+            ESP_LOGD(TAG, "HTTP_EVENT_ON_HEADER, key=%s, value=%s", evt->header_key, evt->header_value);
+            break;
+        case HTTP_EVENT_ON_DATA:
+            ESP_LOGD(TAG, "HTTP_EVENT_ON_DATA, len=%d", evt->data_len);
+            if (!esp_http_client_is_chunked_response(evt->client)) {
+                // Write out data
+                memcpy(http_buffer + http_data_len, evt->data, evt->data_len);
+                http_data_len += evt->data_len;
+                printf("%.*s\n", evt->data_len, (char*)evt->data);
+            }
+
+            break;
+        case HTTP_EVENT_ON_FINISH:
+            ESP_LOGD(TAG, "HTTP_EVENT_ON_FINISH");
+            break;
+        case HTTP_EVENT_DISCONNECTED:
+            ESP_LOGD(TAG, "HTTP_EVENT_DISCONNECTED");
+            break;
+    }
+    return ESP_OK;
+}
+
+esp_err_t _http_event_token_handler(esp_http_client_event_t *evt)
+{
+    esp_http_client_handle_t client = evt->client;
+    const char *device_id = "567c0e98-6f60-4f24-a7dc-e5669636c649";
+    const char *device_secret = "mqqk5t7AS4-KVS5aRaPQDpHmmj8ml8b-a8oMdmFkOOw";
     uint32_t sz = 512;
 
     switch (evt->event_id) {
@@ -87,7 +147,7 @@ esp_err_t _http_event_handler(esp_http_client_event_t *evt)
                 // Write out data
                 memcpy(http_buffer + http_data_len, evt->data, evt->data_len);
                 http_data_len += evt->data_len;
-                printf("%.*s", evt->data_len, (char*)evt->data);
+                printf("%.*s\n", evt->data_len, (char*)evt->data);
             }
 
             break;
@@ -101,48 +161,51 @@ esp_err_t _http_event_handler(esp_http_client_event_t *evt)
     return ESP_OK;
 }
 
-static void here_request(esp_gps_data_t gps_data)
+esp_err_t _http_event_map_handler(esp_http_client_event_t *evt)
 {
+    switch (evt->event_id)
+    {
+    case HTTP_EVENT_ERROR:
+        ESP_LOGD(TAG, "HTTP_EVENT_ERROR");
+        break;
+    case HTTP_EVENT_ON_CONNECTED:
+        ESP_LOGD(TAG, "HTTP_EVENT_ON_CONNECTED");
+        break;
+    case HTTP_EVENT_HEADER_SENT:
+        ESP_LOGD(TAG, "HTTP_EVENT_HEADER_SENT");
+        break;
+    case HTTP_EVENT_ON_HEADER:
+        ESP_LOGD(TAG, "HTTP_EVENT_ON_HEADER, key=%s, value=%s", evt->header_key, evt->header_value);
+        break;
+    case HTTP_EVENT_ON_DATA:
+        // ESP_LOGI(TAG, "HTTP_EVENT_ON_DATA, len=%d", evt->data_len);
+            if (!esp_http_client_is_chunked_response(evt->client)) {
+				memcpy(http_map_buffer + http_map_data_len, evt->data, evt->data_len);
+                http_map_data_len += evt->data_len;
+			}
+        break;
+    case HTTP_EVENT_ON_FINISH:
+        ESP_LOGD(TAG, "HTTP_EVENT_ON_FINISH");
+        break;
+    case HTTP_EVENT_DISCONNECTED:
+        ESP_LOGD(TAG, "HTTP_EVENT_DISCONNECTED");
+        break;
+    }
+    return ESP_OK;
+}
 
+static bool here_request(esp_gps_data_t gps_data)
+{
+    bool ret = false;
     esp_http_client_config_t config = {
         .url = url,
         .event_handler = _http_event_handler,
         .buffer_size = 2048,
     };
-    esp_http_client_handle_t client;
-
-    // esp_http_client_set_header(client, "Content-Type", "application/json");
-    // esp_http_client_set_method(client, HTTP_METHOD_POST);
-
-    http_data_len = 0;
+    esp_http_client_handle_t client = esp_http_client_init(&config);
     esp_err_t err;
-    char *access_token = NULL;
-    int _retry = 3;
-    while (_retry) {
-        ESP_LOGI(TAG, "retry time : %d", _retry);
-        client = esp_http_client_init(&config);
-        esp_http_client_set_method(client, HTTP_METHOD_POST);
-        err = esp_http_client_perform(client);
-        _retry--;
-        if (err == ESP_OK) {
-            http_buffer[http_data_len] = 0;
-            if (esp_http_client_get_status_code(client) == 200) {
-                access_token = json_get_token_value(http_buffer, "accessToken");
-                if (access_token) {
-                    ESP_LOGE(TAG, "Access token = %s", access_token);
-                }
-                break;
-            }
 
-            ESP_LOGI(TAG, "HTTPS Status = %d, content_length = %d",
-                    esp_http_client_get_status_code(client),
-                    esp_http_client_get_content_length(client));
-        } else {
-            ESP_LOGE(TAG, "Error perform http request %s", esp_err_to_name(err));
-        }
-        vTaskDelay(3000 / portTICK_PERIOD_MS);
-    }
-    if (access_token) {
+    if (access_token != NULL) {
         char *token = NULL;
         char *tracking_data = NULL;
         asprintf(&token, "Bearer %s", access_token);
@@ -150,7 +213,7 @@ static void here_request(esp_gps_data_t gps_data)
         esp_http_client_set_method(client, HTTP_METHOD_POST);
         esp_http_client_set_header(client, "Authorization", token);
     
-        ESP_LOGW(TAG, "header=%s", token);
+        ESP_LOGD(TAG, "header=%s", token);
         int data_len = asprintf(&tracking_data, "[{"
             "\"timestamp\": %ld,"
             "\"position\": {"
@@ -158,36 +221,37 @@ static void here_request(esp_gps_data_t gps_data)
                 "\"lng\": %s,"
                 "\"alt\": 1,"
                 "\"accuracy\": 10"
+            "},"
+            "\"payload\": {"
+                "\"temperature\": 25"
+            "},"
+            "\"system\": {"
+                "\"stateVersion\": %d"
             "}"
-        "}]", time(NULL), gps_data.latitude, gps_data.longitude);
+        "}]", time(NULL), gps_data.latitude, gps_data.longitude, state_version);
         char len_str[10];
         sprintf(len_str, "%d", data_len);
-        ESP_LOGI(TAG, "Write data = %s", tracking_data);
+        ESP_LOGD(TAG, "Write data = %s", tracking_data);
         esp_http_client_set_post_field(client, tracking_data, data_len);
         esp_http_client_set_header(client, "Content-Type", "application/json");
         esp_http_client_set_header(client, "Content-Length", len_str);
         http_data_len = 0;
-        _retry = 3;
-        while (_retry) {
-            ESP_LOGI(TAG, "retry time: %d", _retry);
-            _retry--;
             err = esp_http_client_perform(client);
 
             if (err == ESP_OK) {
-        
                 ESP_LOGI(TAG, "HTTPS Status = %d, content_length = %d",
                         esp_http_client_get_status_code(client),
                         esp_http_client_get_content_length(client));
-                break;
+            ret = true;
             } else {
                 ESP_LOGE(TAG, "Error perform http request %s", esp_err_to_name(err));
             }
-        }
         free(tracking_data);
         free(token);
-        free(access_token);
+        // free(access_token);
     }
     esp_http_client_cleanup(client);
+    return ret;
 }
 
 static void http_update_position(esp_gps_data_t gps_data)
@@ -197,7 +261,6 @@ static void http_update_position(esp_gps_data_t gps_data)
     struct tm timeinfo = { 0 };
     int retry = 0;
     const int retry_count = 10;
-
     while (timeinfo.tm_year < (2016 - 1900) && ++retry < retry_count) {
         ESP_LOGI(TAG, "Waiting for system time to be set... (%d/%d)", retry, retry_count);
         vTaskDelay(2000 / portTICK_PERIOD_MS);
@@ -205,7 +268,15 @@ static void http_update_position(esp_gps_data_t gps_data)
         localtime_r(&now, &timeinfo);
     }
     vTaskDelay(1000 / portTICK_PERIOD_MS);
-    here_request(gps_data);
+    int _retry = 3;
+    while (_retry) {
+        ESP_LOGI(TAG, "try: %d", _retry);
+        if (here_request(gps_data)) {
+            break;
+        }
+        vTaskDelay(3000 / portTICK_PERIOD_MS);
+        _retry--;
+    }
     ESP_LOGI(TAG, "Finish http example");
     // vTaskDelete(NULL);
 }
@@ -226,6 +297,28 @@ bool network_is_availablde() {
     return false;
 }
 
+void display_map(esp_gps_data_t gps_data)
+{
+    char* tracking_data = NULL;
+    int data_len = asprintf(&tracking_data, MAP_TEMPLATE, gps_data.latitude, gps_data.longitude);
+    tracking_data[data_len] = 0;
+    ESP_LOGI(TAG, "map req: %s", tracking_data);
+    esp_http_client_config_t config = {
+        .url = "https://image.maps.api.here.com/mia/1.6/tiltmap?app_id=4SOW7wtvnVNhniJUSFRr&app_code=LAQ7xdmuHxFmTlPhb_8Heg&w=320&h=240&sb=mk&z=15&t=2&u=100&c=10.8556048,106.7886128",
+        .event_handler = _http_event_map_handler,
+    };
+    esp_http_client_handle_t client = esp_http_client_init(&config);
+    http_map_data_len = 0;
+    esp_err_t err = esp_http_client_perform(client);
+    if (err == ESP_OK) {
+		TFT_jpg_image(CENTER, CENTER, 0, NULL, http_map_buffer, http_map_data_len); 
+    } else {
+        ESP_LOGE(TAG, "HTTP GET request failed: %s", esp_err_to_name(err));
+    }
+    esp_http_client_cleanup(client);
+    ESP_LOGI(TAG, "Display complete %d", http_map_data_len);
+}
+
 void app_main()
 {
     esp_err_t ret = nvs_flash_init();
@@ -235,43 +328,53 @@ void app_main()
     }
     ESP_ERROR_CHECK( ret );
     tcpip_adapter_init();
-    // TFT_PinsInit();
-    // spi_lobo_device_handle_t spi;
+
+    tft_disp_type = DEFAULT_DISP_TYPE;
+	_width = DEFAULT_TFT_DISPLAY_WIDTH;  // smaller dimension
+	_height = DEFAULT_TFT_DISPLAY_HEIGHT; // larger dimension
+	max_rdclock = 8000000;
+    TFT_PinsInit();
+    spi_lobo_device_handle_t spi;
 	
-    // spi_lobo_bus_config_t buscfg={
-    //     .miso_io_num=PIN_NUM_MISO,				// set SPI MISO pin
-    //     .mosi_io_num=PIN_NUM_MOSI,				// set SPI MOSI pin
-    //     .sclk_io_num=PIN_NUM_CLK,				// set SPI CLK pin
-    //     .quadwp_io_num=-1,
-    //     .quadhd_io_num=-1,
-	// 	.max_transfer_sz = 6*1024,
-    // };
-    // spi_lobo_device_interface_config_t devcfg={
-    //     .clock_speed_hz=8000000,                // Initial clock out at 8 MHz
-    //     .mode=0,                                // SPI mode 0
-    //     .spics_io_num=-1,                       // we will use external CS pin
-	// 	.spics_ext_io_num=PIN_NUM_CS,           // external CS pin
-	// 	.flags=LB_SPI_DEVICE_HALFDUPLEX,        // ALWAYS SET  to HALF DUPLEX MODE!! for display spi
-    // };
-    // ret=spi_lobo_bus_add_device(SPI_BUS, &buscfg, &devcfg, &spi);
-    // assert(ret==ESP_OK);
-	// printf("SPI: display device added to spi bus (%d)\r\n", SPI_BUS);
-	// disp_spi = spi;
+    spi_lobo_bus_config_t buscfg={
+        .miso_io_num=PIN_NUM_MISO,				// set SPI MISO pin
+        .mosi_io_num=PIN_NUM_MOSI,				// set SPI MOSI pin
+        .sclk_io_num=PIN_NUM_CLK,				// set SPI CLK pin
+        .quadwp_io_num=-1,
+        .quadhd_io_num=-1,
+		.max_transfer_sz = 6*1024,
+    };
+    spi_lobo_device_interface_config_t devcfg={
+        .clock_speed_hz=8000000,                // Initial clock out at 8 MHz
+        .mode=0,                                // SPI mode 0
+        .spics_io_num=-1,                       // we will use external CS pin
+		.spics_ext_io_num=PIN_NUM_CS,           // external CS pin
+		.flags=LB_SPI_DEVICE_HALFDUPLEX,        // ALWAYS SET  to HALF DUPLEX MODE!! for display spi
+    };
 
-	// // ==== Test select/deselect ====
-	// ret = spi_lobo_device_select(spi, 1);
-    // assert(ret==ESP_OK);
-	// ret = spi_lobo_device_deselect(spi);
-    // assert(ret==ESP_OK);
+	ret=spi_lobo_bus_add_device(SPI_BUS, &buscfg, &devcfg, &spi);
+	disp_spi = spi;
 
-	// printf("SPI: attached display device, speed=%u\r\n", spi_lobo_get_speed(spi));
-	// printf("SPI: bus uses native pins: %s\r\n", spi_lobo_uses_native_pins(spi) ? "true" : "false");
-    // TFT_display_init();
+	TFT_display_init();
+	max_rdclock = find_rd_speed();
+	font_rotate = 0;
+	text_wrap = 0;
+	font_transparent = 0;
+	font_forceFixed = 0;
+	gray_scale = 0;
+    TFT_setGammaCurve(DEFAULT_GAMMA_CURVE);
+	TFT_setRotation(LANDSCAPE);
+	TFT_setFont(DEJAVU24_FONT, NULL);
+	TFT_resetclipwin();
+
+    TFT_print("Smart Tracker", CENTER, CENTER);
+
+    vTaskDelay(3000 / portTICK_PERIOD_MS);
 
     esp_gps_config_t esp_gps_config = {
         .uart_baudrate = 9600,
         .uart_tx_pin = 19,
-        .uart_rx_pin = 18,
+        .uart_rx_pin = 16,
         .uart_port_num = UART_NUM_2,
     };
     esp_gps_handle_t gps_handle = esp_gps_init(&esp_gps_config);
@@ -295,28 +398,37 @@ void app_main()
 #endif
     vTaskDelay(2000 / portTICK_PERIOD_MS);
     initialize_sntp();
-    // xTaskCreate(&http_update_task, "http_update_task", 8192, NULL, 5, NULL);
-    char fmt_buf[32];
     esp_http_client_config_t config = {
-        .url = "https://image.maps.api.here.com/mia/1.6/routing?app_id=2pt1MIM3bIHER2qfIM3M&app_code=IfhWsZtLJEZKDArU9yxSDg&waypoint0=10.767317,106.641038&waypoint1=10.782447,106.660454&lc=1652B4&lw=6&t=0&ppi=320&w=320&h=240",
-        .event_handler = _http_event_handler,
+        .url = url,
+        .event_handler = _http_event_token_handler,
+        .buffer_size = 2048,
     };
-    // esp_http_client_handle_t client = esp_http_client_init(&config);
+    esp_http_client_handle_t client = esp_http_client_init(&config);
 
-    // // GET
-    // esp_err_t err = esp_http_client_perform(client);
-    // if (err == ESP_OK) {
-    //     ESP_LOGI(TAG, "HTTP GET Status = %d, content_length = %d",
-    //     esp_http_client_get_status_code(client),
-    //     esp_http_client_get_content_length(client));
-    //     uint8_t *buff = malloc(40000);
-    //     esp_http_client_read(client, (char*)buff, 40000);
-    //     TFT_jpg_image(0, 0, 1, NULL, buff, 0);
-    //     free(buff);
-    // } else {
-    //     ESP_LOGE(TAG, "HTTP GET request failed: %s", esp_err_to_name(err));
-    // }
-    // esp_http_client_cleanup(client);
+    // esp_http_client_set_header(client, "Content-Type", "application/json");
+    esp_http_client_set_method(client, HTTP_METHOD_POST);
+
+    http_data_len = 0;
+    esp_err_t err = esp_http_client_perform(client);
+
+    access_token = NULL;
+    if (err == ESP_OK) {
+        http_buffer[http_data_len] = 0;
+        if (esp_http_client_get_status_code(client) == 200) {
+            access_token = json_get_token_value(http_buffer, "accessToken");
+            if (access_token) {
+                ESP_LOGI(TAG, "Access token = %s", access_token);
+            }
+        }
+
+        ESP_LOGI(TAG, "HTTPS Status = %d, content_length = %d",
+                esp_http_client_get_status_code(client),
+                esp_http_client_get_content_length(client));
+    } else {
+        ESP_LOGE(TAG, "Error perform http request %s", esp_err_to_name(err));
+    }
+    esp_http_client_cleanup(client);
+    char fmt_buf[32];
     while(1){
         esp_gps_data_t gps_data;
         bool ret = esp_gps_read_data(gps_handle, &gps_data);
@@ -325,13 +437,10 @@ void app_main()
             ESP_LOGI(TAG, "Latitude: %s\n", gps_data.latitude);
             strftime(fmt_buf, sizeof(fmt_buf), "%H:%M:%S", &gps_data.time);
             ESP_LOGI(TAG, "Time: %s\n", fmt_buf);
-            // time_t t = mktime(&gps_data.time);
-            // struct timeval now = { .tv_sec = t };
-            // settimeofday(&now, NULL);
             if (network_is_availablde()) {
                 http_update_position(gps_data);
             }
-            // http_update_position(gps_data);
+            display_map(gps_data);
         } else {
             ESP_LOGI(TAG, "Data not available!");
         }
